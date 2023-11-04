e06fd32b51a4ca1ea08fb12367f69ead86bd60bc
Dennis Kimani <dxaldovah@gmail.com>
2023-11-03 19:35:28 -0700

    will update later

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  const selectedAmenities = {};
+
+  $('input[type="checkbox"]').change(function() {
+    const amenityId = $(this).data("id");
+    const amenityName = $(this).data("name");
+
+    if (this.checked) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selectedAmenities[amenityId];
+    }
+    const amenityList = Object.values(selectedAmenities).join(', ');
+
+    $('div.Amenities h4').text(amenityList);
+  });
+
+  const apiStatusDiv = $('#api_status');
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+      apiStatusDiv.addClass('available');
+    } else {
+      apiStatusDiv.removeClass('available');
+    }
+  });
+});
